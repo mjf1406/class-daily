107c38fb9ae9bc7ecd17b421b9920a77b63a6821
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2024-01-10 15:11:20 +0900

    initial set up complete, NO FUNCTIONS

---
+++ js/helpers/tippys.js
@@ -0,0 +1,20 @@
+tippy('#centers', {
+    content: "Rotations"
+})
+tippy('#settings', {
+    content: "Settings"
+})
+tippy('#add-custom-timer', {
+    content: "Create custom timer"
+})
+tippy('#custom-timer-settings', {
+    content: "Custom timers settings"
+})
+tippy('#rotation-duration-help', {
+    content: "Ctrl + Click to adjust by 5 <br> Shift + Click to adjust by 10",
+    allowHTML: true
+})
+tippy(document.querySelectorAll('i[name="number-input-help"]'), {
+    content: "Ctrl + Click to adjust by 5 <br> Shift + Click to adjust by 10",
+    allowHTML: true
+})
