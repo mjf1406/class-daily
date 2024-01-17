cfcc238fd4f9e10915ab56dca951b0f69fcc74cb
Michael Fitzgerald <126652136+mjf1406@users.noreply.github.com>
2024-01-17 11:34:29 +0900

    Add files via upload

---
+++ listeners.js
@@ -0,0 +1,25 @@
+const createClassModal = document.getElementById('create-class-modal')
+const createClassButton = document.getElementById('save-create-class')
+createClassButton.addEventListener('click', function(event){
+    event.preventDefault()
+    const name = document.getElementById('create-class-name-input')
+    const color = document.getElementById('create-class-color')
+    const shape = getSelectedValueFromRadioGroup('create-class-shape')
+    addClass(name.value, color.value, shape)
+
+    makeToast(`The <b>${name.value}</b> class was created successfully!`, 'success')
+
+    createClassModal.classList.toggle('hidden')
+
+    name.value = ''
+    color.value = '#000'
+    setSelectedValueFromRadioGroup('create-class-shape', 'xmark')
+})
+
+classesSelect.addEventListener('change', function(){
+    const selectedClass = this.value
+    console.log("🚀 ~ classesSelect.addEventListener ~ selectedClass:", selectedClass)
+    fetchCSV('../SCHEDULE - Sheet1').then(data => {
+        populateActivities(data, selectedClass)
+    })
+})
